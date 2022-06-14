1820a392b4b823d9f86b18d8a1b649a919da2bd1
BURG3R5 <77491630+BURG3R5@users.noreply.github.com>
2022-06-14 11:05:36 +0530

    docs(setup): Document `parse` method and helpers.

--- encrypted_search/index.py
+++ encrypted_search/index.py
@@ -7,12 +7,32 @@ from utils.types import corpus_type, event_type
 
 
 class EncryptedIndex:
+    """A searchable obfuscated index.
+
+    To prevent data leakage, we obfuscate a simple inverted index into two structures: a collection of arrays and a lookup table. This class sets up this scheme and exposes the data to be searched upon.
+
+    Args:
+        events: List of Matrix room events to be indexed
+
+    Attributes:
+        database: Three dimensional array containing document ids according to obfuscation scheme
+        lookup_table: Map from a keyword to corresponding location in the database
+    """
 
     def __init__(self, events: List[event_type]):
         documents, keywords = self.parse(events)
 
     @staticmethod
     def parse(events: List[event_type]) -> Tuple[corpus_type, Set[str]]:
+        """Transforms raw Matrix room events into normalized and simplified documents.
+
+        Args:
+            events: List of Matrix room events to be indexed
+
+        Returns:
+            A tuple of the form (D, K), where — D is a mapping from document ids to the set of keywords present in each document and K is a set of all the normalized tokens present in the corpus.
+        """
+
         documents: corpus_type = {}
         for event in events:
             if (event["type"] == "m.room.message" and "content" in event
--- utils/test_helpers.py
+++ utils/test_helpers.py
@@ -3,6 +3,18 @@ import os
 
 
 def get_test_case(test: str, case: str):
+    """Fetches data for a test case.
+
+    Data is stored in JSON files like tests/data/<class-name>/<method-name>/<case>.json
+
+    Args:
+        test: String of the form A/B where A is the name of the class and B is the name of the method to be tested
+        case: String representing which particular scenario is being tested
+
+    Returns:
+        IN and OUT data for the requested test case.
+    """
+
     file_path = f"tests/data/{test}/{case}.json"  # Run from root
     if os.path.exists(file_path):
         with open(file_path, encoding="utf-8") as file:
